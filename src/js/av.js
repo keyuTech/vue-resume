78d9574436a880a3062fbae3a12148f2453b4fb3
wky0615 <wky_0615@163.com>
2018-04-14 22:55:31 +0800

    初始化 leanCloud

---
+++ src/js/av.js
@@ -0,0 +1,15 @@
+var APP_ID = '72T0UPzsD5Ph5hrqYOIJNqNf-gzGzoHsz';
+    var APP_KEY = '8PxsDou6WajrAvHMj6VkzcAU';
+
+    AV.init({
+      appId: APP_ID,
+      appKey: APP_KEY
+    });
+
+    var TestObject = AV.Object.extend('TestObject');
+    var testObject = new TestObject();
+    testObject.save({
+      words: 'Hello World!'
+    }).then(function (object) {
+      alert('LeanCloud Rocks!');
+    })
